d9dc03f29e3280dabaf67e848f1da58114a06aed
Gregory A. Kosinovsky <kosinov@quartz2300.llnl.gov>
2020-09-08 10:56:43 -0700

    fixed cmake/ER_ADD_TEST.cmake

--- cmake/ER_ADD_TEST.cmake
+++ cmake/ER_ADD_TEST.cmake
@@ -1,7 +1,11 @@
 function(ER_ADD_TEST name args outputs)
 
   # job launcher
-  if(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
+  if(NOT DEFINED VELOC_RESOURCE_MANAGER)
+    set(test_param mpirun -np 3)
+  elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
+    set(test_param mpirun -np 3)
+  elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
     set(test_param jsrun -r 1)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "SLURM")
     set(test_param srun -N 3 -n 3)
